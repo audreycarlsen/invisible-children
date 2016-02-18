fba071e28d8f4d35cb6c94bfadd8018a5f6b947b
Audrey Carlsen <acarlsen@seattletimes.com>
2016-02-17 16:22:45 -0800

    add images

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,17 @@
 // var track = require("./lib/tracking");
 
 require("component-responsive-frame/child");
+var qsa = require("./lib/qsa");
+var animateScroll = require("./lib/animateScroll");
+
+qsa(".subhed a").forEach(function(a) {
+  a.addEventListener("click", function(e) {
+    var href = this.getAttribute("href");
+    if (href.indexOf("#") != 0) return;
+    var section = document.querySelector(href);
+    if (!section) return;
+    e.preventDefault();
+    animateScroll(section);
+    window.history.pushState(href, href, href);
+  });
+});
